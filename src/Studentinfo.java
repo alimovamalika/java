3ea929c41864f9b1e34461f9d4d0120d731c1abe
Malika <alimova.malika@icloud.com>
2026-01-22 13:47:51 +0500

    Добавлена Лабораторная работа №1

---
+++ src/Studentinfo.java
@@ -0,0 +1,29 @@
+public class Studentinfo // Имя файла должно совпадать с именем класса
+// Файл: StudentInfo.java
+public class StudentInfo {
+    // Метод main — точка входа в Java-программу
+    public static void main(String[] args) {
+        // Объявление и инициализация переменной для хранения ФИО студента
+        [span_2](start_span)String fullName = "Иванов Иван Иванович";[span_2](end_span)
+
+                // Объявление переменной для хранения номера группы
+                [span_3](start_span)String group = "ИС-2204";[span_3](end_span)
+
+                // Объявление переменной для хранения образовательной программы
+                [span_4](start_span)String program = "Информационные системы";[span_4](end_span)
+
+                // Вывод заголовка в консоль
+                [span_5](start_span)System.out.println("=== Информация о студенте ===");[span_5](end_span)
+
+                // Вывод ФИО студента
+                [span_6](start_span)System.out.println("ФИО: " + fullName);[span_6](end_span)
+
+                // Вывод группы студента
+                [span_7](start_span)System.out.println("Группа: " + group);[span_7](end_span)
+
+                // Вывод образовательной программы
+                [span_8](start_span)System.out.println("ОП/Специальность: " + program);[span_8](end_span)
+    }
+}
+{
+}
